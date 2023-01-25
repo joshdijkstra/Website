286abea2dba7738f2452770cc64b5bbdf5f78abd
Josh Dykstra <josh_dykstra98@hotmail.co.uk>
2023-01-25 12:03:31 +0000

    pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MusicPageComponent } from './music-page/music-page.component';
+import { ArtPageComponent } from './art-page/art-page.component';
+import { CodingPageComponent } from './coding-page/coding-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     PagenotfoundComponent,
-    HeaderComponent
+    HeaderComponent,
+    MusicPageComponent,
+    ArtPageComponent,
+    CodingPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
